405f4f98eff928cd1ed5bf35417178fbed4a3970
dessertivore <landau.sophie@gmail.com>
2023-10-24 20:46:53 +0100

    Added hashtable with dunders

--- lesson2_assignment.py
+++ lesson2_assignment.py
@@ -37,7 +37,7 @@ class HashTable:
             return None
         # else, return key value pair from that index
         # after making sure that key matches key at index
-        keyfound, x = self.data_list[idx]
+        keyfound = self.data_list[idx][0]
         if keyfound == key:
             self.data_list[idx] = (key, value)
 
@@ -134,3 +134,89 @@ def get_valid_index(data_list, key):
             idx = 0
             
             """
+
+
+"""
+Make a python friendly hash table interface
+"""
+
+
+class HashTableImproved:
+    def __init__(self, max_size=4096):
+        self.data_list = [None] * max_size
+
+    def get_valid_index(self, key):
+        idx = hash(key) % len(self.data_list)
+        while True:
+            # Get the key-value pair stored at idx
+            kv = self.data_list[idx]
+            # If it is None, return the index
+            if kv == None:
+                return idx
+            # If the stored key matches the given key, return the index
+            if kv[0] == key:
+                return idx
+            # Move to the next index
+            idx += 1
+            # Go back to the start if you have reached the end of the array
+            if idx == len(self):
+                idx = 0
+
+    def __getitem__(self, key):
+        idx = self.get_valid_index(key)
+        initial_idx = idx  # Store the initial index to check if we've looped through the entire table
+        # keep looping until reaches return statement
+        while True:
+            # If there is no index associated with key, return None
+            if self.data_list[idx] is None:
+                return None
+            # Otherwise, check if the key matches the key at the current index
+            if self.data_list[idx][0] == key:
+                return self.data_list[idx][
+                    1
+                ]  # Return the value associated with the key
+            # If the key doesn't match, it might be due to collision; move to the next index
+            idx = (idx + 1) % len(self.data_list)
+            # If we've checked all possible indices, return None to avoid an infinite loop
+            if idx == initial_idx:
+                return None
+
+    def __setitem__(self, key, value):
+        # check if key exists
+        # if not, create new entry
+        if self[key] == None:
+            self.data_list[self.get_valid_index(key)] = (key, value)
+            # if entry exists, replace entry at key's index with new value
+        if self[key]:
+            idx = self.get_valid_index(key)
+            self.data_list[idx] = key, value
+
+    def __iter__(self):
+        # to call this, use 'for x in example, print(x)'
+        # ignores None values
+        filtered_items = [x for x in self.data_list if x is not None]
+        return iter(filtered_items)
+
+    def __len__(self):
+        return len([x for x in self])
+
+    def __repr__(self):
+        from textwrap import indent
+
+        pairs = [
+            indent("{} : {}".format(repr(kv[0]), repr(kv[1])), "  ") for kv in self
+        ]
+        return "{\n" + "{}".format(",\n".join(pairs)) + "\n}"
+
+    def __str__(self):
+        return repr(self)
+
+
+example1 = HashTableImproved()
+example1["jimK"] = "James Tiberius Kirk"
+example1["jimk"] = "Jamie Tiberius Kirk"
+example1["Kjim"] = "Kirk James Tiberius"
+example1["nurseC"] = "Nurse Christine Chapel"
+print(example1["Kjim"])
+for x in example1:
+    print(x)
